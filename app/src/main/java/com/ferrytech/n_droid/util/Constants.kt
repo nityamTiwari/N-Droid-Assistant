949100d5953c2ec8a2910388f612f32f4bcce20e
Nityam Tiwari <153179834+nityamTiwari@users.noreply.github.com>
2026-03-08 12:52:37 +0530

    Clear Constants for API key and model name
    Removed API key and model name from Constants.

--- app/src/main/java/com/ferrytech/n_droid/util/Constants.kt
+++ app/src/main/java/com/ferrytech/n_droid/util/Constants.kt
@@ -1,6 +1,7 @@
 package com.ferrytech.n_droid.util
 
 object Constants {
-    const val GEMINI_API_KEY = "AIzaSyCLaPCdGzhJdeRtE2pryWvKl15icRa9B8Q"
-    const val MODEL_NAME = "gemini-3-flash-preview"
+    const val GEMINI_API_KEY = ""
+    const val MODEL_NAME = ""
+
 }
