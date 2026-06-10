68691e5c99f77bba2acedcf0f4380a6f8cc11a78
Nityam <nityamtiwari08@gmail.com>
2026-06-11 03:18:21 +0530

    #9 initalize databse in application class

--- app/src/main/java/com/ferrytech/n_droid/NDroidApp.kt
+++ app/src/main/java/com/ferrytech/n_droid/NDroidApp.kt
@@ -1,9 +1,12 @@
 package com.ferrytech.n_droid
 
 import android.app.Application
+import com.ferrytech.n_droid.data.local.DatabaseProvider
 
 class NDroidApp : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        DatabaseProvider.init(this)
     }
 }
